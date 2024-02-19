6578c0359def04382f12b673cecc50329ac3e20d
remreim <studen_studen@yahoo.com>
2024-02-19 19:17:14 +0200

    first commit

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+//https://www.youtube.com/watch?v=uC7vajbnZS4
+//MSBuild With Jenkins | Jenkins For C# / .NET Applications | Thetips4you
+//atlikta iki 4:10
+
+
+
+using System;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            Console.WriteLine("Hello World! from Thetips4you");
+        }
+    }
+}
